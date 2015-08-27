530efba77c3e111609c62afba8c6512861ddedd2
Bernard Shyu <bernard_shyu@bizlinktech.com>
2015-08-27 23:09:43 +0800

    dfu: biz_rel_image.sh with ID & SHA1 info
    Improve the FW release script: biz_rel_image.sh
    1. For every build, the image will be exactly the same (by 'USE_GIT_DATE=1')
    2. Generate SHA1, retrieve PID & DID from the board file, get TODAY,
       to form the FW Image's file name
    
    BUG=none
    BRANCH=biz/master
    TEST= biz_rel_image.sh <board>

--- extern/bin/biz_rel_image.sh
+++ extern/bin/biz_rel_image.sh
@@ -3,15 +3,26 @@
 BOARD=$1
 
 if [ ! -f build/$BOARD/ec.RW.bin ]; then
-	echo -e "The board $BOARD hasn't been built"
-	exit 1
+	if ! id | grep chronos  > /dev/null 2>&1; then
+		echo -e "The board $BOARD hasn't been built"
+		exit 1
+	fi
+	make clobber
+	make -j BOARD=$BOARD USE_GIT_DATE=1
 fi
 
+PID=`egrep -r '^#define *CONFIG_USB_PID'     board/$BOARD | sed -e 's/.*_USB_PID *\([^ ]*\).*/\1/' -e 's/0x//'`
+DID=`egrep -r '^#define *CONFIG_USB_BCD_DEV' board/$BOARD | sed -e 's/.*_USB_BCD_DEV *\([^ ]*\).*/\1/' -e 's/0x//'`
+
 if egrep -r '^#define *BIZCFG_ENTER_DFU_VIA_BILLBOARD' board/$BOARD > /dev/null 2>&1; then
-	echo -e "Release FW with Bizlink DFU/BOOT mode"
-	cat extern/release/RO_DfuBoot.bin build/$BOARD/ec.RW.bin > /tmp/ec.$BOARD-rel.bin
+	echo -e "\n\nRelease FW with Bizlink DFU/BOOT mode, PID=$PID DID=$DID"
+	cat extern/release/RO_DfuBoot.bin build/$BOARD/ec.RW.bin > /tmp/ec-rel.$BOARD.bin
 else
-	echo -e "Release FW with Google GFU VDM mode"
-	cp -v build/$BOARD/ec.bin /tmp/ec.$BOARD-rel.bin
+	echo -e "\n\nRelease FW with Google GFU VDM mode, PID=$PID DID=$DID"
+	cp -v build/$BOARD/ec.bin /tmp/ec-rel.$BOARD.bin
 fi
+
+echo -e "\nSHA1: `sha1sum /tmp/ec-rel.$BOARD.bin`\n"
+SHA1=`sha1sum /tmp/ec-rel.$BOARD.bin | cut -b 1-8`
+cp -v /tmp/ec-rel.$BOARD.bin /tmp/ec-rel.$BOARD.ID-$PID-$DID.`date +%Y-%m%d`.$SHA1.bin
 
