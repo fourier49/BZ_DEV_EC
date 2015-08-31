013ed7211270f84162516a8e4d868a9d88d2c536
Bernard Shyu <bernard_shyu@bizlinktech.com>
2015-08-31 10:12:20 +0800

    dfu: biz_rel_image.sh for non-Alt-Mode
    Takes care of both the flags: BIZCFG_ENTER_DFU_VIA_BILLBOARD and
    BIZCFG_BILLBOARD_ALWAYS_CONNECT, for the 3 types of FW update mechanism.
    
    BUG=none
    BRANCH=biz/master
    TEST= biz_rel_image.sh <board>

--- extern/bin/biz_rel_image.sh
+++ extern/bin/biz_rel_image.sh
@@ -2,13 +2,13 @@
 
 BOARD=$1
 
-if [ ! -f build/$BOARD/ec.RW.bin ]; then
-	if ! id | grep chronos  > /dev/null 2>&1; then
-		echo -e "The board $BOARD hasn't been built"
-		exit 1
-	fi
+if id | grep chronos  > /dev/null 2>&1; then
 	make clobber
 	make -j BOARD=$BOARD USE_GIT_DATE=1
+fi
+if [ ! -f build/$BOARD/ec.RW.bin ]; then
+	echo -e "The board $BOARD hasn't been built"
+	exit 1
 fi
 
 PID=`egrep -r '^#define *CONFIG_USB_PID'     board/$BOARD | sed -e 's/.*_USB_PID *\([^ ]*\).*/\1/' -e 's/0x//'`
@@ -16,6 +16,11 @@ DID=`egrep -r '^#define *CONFIG_USB_BCD_DEV' board/$BOARD | sed -e 's/.*_USB_BCD
 
 if egrep -r '^#define *BIZCFG_ENTER_DFU_VIA_BILLBOARD' board/$BOARD > /dev/null 2>&1; then
 	echo -e "\n\nRelease FW with Bizlink DFU/BOOT mode, PID=$PID DID=$DID"
+	if egrep -r '^#define *BIZCFG_BILLBOARD_ALWAYS_CONNECT' board/$BOARD > /dev/null 2>&1; then
+		echo -e "USB/BB always ON, can update on any Windows"
+	else
+		echo -e "can only be updated on non-Alt-Mode Windows"
+	fi
 	cat extern/release/RO_DfuBoot.bin build/$BOARD/ec.RW.bin > /tmp/ec-rel.$BOARD.bin
 else
 	echo -e "\n\nRelease FW with Google GFU VDM mode, PID=$PID DID=$DID"
